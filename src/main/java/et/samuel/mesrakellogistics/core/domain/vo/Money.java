050ebf4cbc6b32b15623f2eac064a94fdd523d36
samuel <saa2280@cumc.columbia.edu>
2026-03-11 17:08:52 +0300

    implementing domain

--- src/main/java/et/samuel/mesrakellogistics/core/domain/vo/Money.java
+++ src/main/java/et/samuel/mesrakellogistics/core/domain/vo/Money.java
@@ -1,4 +1,21 @@
 package et.samuel.mesrakellogistics.core.domain.vo;
 
+import lombok.Value;
+
+import java.math.BigDecimal;
+
+@Value
 public class Money {
+    BigDecimal amount;
+    String currency;
+
+    public Money plus(Money other) {
+        return new Money(amount.add(other.amount), currency);
+    };
+    public boolean isGreaterThan(Money other) {
+        return amount.compareTo(other.amount) >= 0;
+    }
+    public static Money zero(String currency) {
+        return new Money(BigDecimal.ZERO, currency);
+    }
 }
--- src/main/java/et/samuel/mesrakellogistics/core/domain/vo/ParcelDimensions.java
+++ src/main/java/et/samuel/mesrakellogistics/core/domain/vo/ParcelDimensions.java
@@ -1,4 +1,17 @@
 package et.samuel.mesrakellogistics.core.domain.vo;
 
+import lombok.Value;
+
+import java.math.BigDecimal;
+
+@Value
 public class ParcelDimensions {
+    Double length;
+    Double width;
+    Double height;
+    String unit;
+
+    public Double volume() {
+        return length * width * height;
+    }
 }
